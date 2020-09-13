2e3d2f1f25d8e8015d1a3992a888a305c022e81a
Marvin Isaac <marvin@marvinisaac.com>
2020-09-14 07:04:55 +0800

    :construction: Part 7: Access one or all tasks with REST-style API
    :wrench: Also add .htaccess file to project

--- api/task/index.php
+++ api/task/index.php
@@ -17,6 +17,45 @@
             header('Content-Type: application/json');
             break;
         }
+
+        case 'GET': {
+            $id = getTaskId();
+            if ($id === null) {
+                header("HTTP/1.0 200 OK");
+                header('Content-Type: application/json');
+                echo json_encode($tasks);
+                break;
+            }
+
+            if (isset($tasks[$id])) {
+                header("HTTP/1.0 200 OK");
+                header('Content-Type: application/json');
+                echo json_encode($tasks[$id]);
+                break;
+            }
+
+            header("HTTP/1.0 404 Not Found");
+            break;
+        }
+
+        default: {
+            header("HTTP/1.0 405 Method Not Allowed");
+            break;
+        }
+    }
+
+    function getTaskId () {
+        // Source: https://developer.okta.com/blog/2019/03/08/simple-rest-api-php#:~:text=$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        $uri = explode( '/', $uri );
+        $id = null;
+        if (isset($uri[4]) &&
+            $uri[4] !== ''
+        ) {
+            $id = $uri[4] - 1;
+        }
+        
+        return $id;
     }
 
     function getTasks () {
